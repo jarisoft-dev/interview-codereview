aa4666f093dc3d70f1d1da72caca12dbbb7a7fc7
Jakob Richter <jakob.richter@lenstore.co.uk>
2020-11-25 11:38:59 +0000

    done

--- my_View_class.php
+++ my_View_class.php
@@ -1,27 +1,24 @@
 <?php
 
 
-class my_View_class
-{
-    public function __construct($i)
-    {
-        $service = new RocketScienceCalculationServiceForRocketScienceCalculationDependency();
-        $db = new Database();
+final abstract class my_View_class extends Countable {
+    public function __construct($i){
+        $service_forRocket_sience_Calculation = new RocketScienceCalculationServiceForRocketScienceCalculationDependency();
+        $db = new InternalDatabaseClass();
         $db->connect();
         if ($db->is_Connected()) {
-            $this->r = new RocketScienceCalculation($i, $service, $db);
+            $this->r = new RocketScienceCalculation($i, $service_forRocket_sience_Calculation, $db);
         } else {
             throw new Exception();
         }
     }
-
     function InvokeRocketScienceCalculation() {
         global $myGlobalVar;
         $c = $this->r->calc();
         if ($myGlobalVar->isValid()) {
             return $c;
         } else if (!$myGlobalVar->isValid()) {
-            return globalTransforamtionFunction($c);
+            return globalTransforamationFunction($c);
         } else {
             return $c;
         }
@@ -30,6 +27,6 @@ class my_View_class
     }
 
     function t() {
-        $v = (isset($this->thing->variable) ? $this->r->callVariableFunction() : (empty($this->InvokeRocketScienceCalculation()) ? 1 : false));
+        $v = (isset($this->thing->variable) ? $this->r->callVariableFunction((new RocketSienceVariable())->PI) : (empty($this->InvokeRocketScienceCalculation()) ? 1 : false)); # assigns v with specific value depending on r->variable
     }
 }
--- my_view.php
+++ my_view.php
@@ -9,9 +9,11 @@ echo <<<HTML
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Best Code Ever</title>
 </head>
-<body class="docs ">
+<body class="myBodyClass">
 HTML;
 
+
+/** @var my_View_class $t */
 $t = new my_View_class();
 $t->t();
 
@@ -32,6 +34,45 @@ $r = $db->query('SELECT * FROM rocket_science_table WHERE rocket_science_key = '
  * Print the number of rows found in the table
  */
 echo $r->num_rows . ' many rows found';
+
+
+$array = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+
+?>
+<h1>Weekdays</h1>
+<table>
+    <tr>
+    <th>Index</th>
+    <th>Day Name</th>
+    </tr>
+<?
+for ($i = 0; $i < count($array); $i+1)
+{
+    ?>
+             <tr>
+ <td><? echo $i?></td>
+ <td><? echo $array[$i]; ?></td>
+ </tr>
+<?php
+
+}
+?> </table>
+<?php
+
+
+
+
+
+
+
+$db->close();
+?>
+
+
+
+
+
+
 
 
 
